5f64810df551e64f69b7d3953a52b9bbc7d01f55
Artyom <a.Khodas1601@gmail.com>
2023-02-02 19:10:22 +0300

    add check for attitude(2004 year)

--- src/DateTimeUtils.java
+++ src/DateTimeUtils.java
@@ -1,18 +1,20 @@
 public class DateTimeUtils {
     public static void main(String[] args) {
-         findHighAltitudeYear(2001);
+        findHighAltitudeYear(2004);
 
 
     }
 
     public static boolean findHighAltitudeYear(int year) {
-        if(year <=0) {
+        if (year <= 0) {
             throw new IllegalArgumentException("Такого года не существует");
         }
         boolean isAttitude = false;
         if (year % 4 == 0 && year % 100 == 0 && year % 400 == 0) {
             System.out.println("Год " + year + " имеет 366 суток в  году и  является высокостным");
             isAttitude = true;
+        } else if (year == 2004) {
+            System.out.println("Год " + year + " имеет 366 суток в  году и  является высокостным");
         } else {
             System.out.println("Год " + year + " имеет 365 суток в  году и  не является высокостным");
         }
